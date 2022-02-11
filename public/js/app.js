f39e60b441abb5bef0f7a6e8cb47fc099224722d
hypercyte <hypercyte@gmail.com>
2022-02-11 17:08:34 +0000

    singleplayer button functionality (opens game menu with bots slider)

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,41 @@
  * RAF
  * Mujahid "hypercyte" Ahmed
  */
+
+// Home button
+const homeButton = document.getElementById('homeButton');
+
+// Singleplayer or "Play With Bots" button
+const singleplayerButton = document.getElementById('singleplayerButton');
+
+// Multiplayer button
+const mutliplayerButton = document.getElementById('multiplayerButton');
+
+// Singleplayer game settings menu
+const singleplayerSettings = document.querySelector('.singleplayerSettings');
+
+// Singleplayer button click listener
+singleplayerButton.addEventListener('click', () => {
+    openSingleplayerMenu();
+    console.log("buttoin clicked and run")
+})
+
+// Home button click listener
+homeButton.addEventListener('click', () => {
+    if (singleplayerSettings.style.display === 'none') return;
+    closeSingleplayerMenu();
+})
+
+// Open singleplayer settings and hide gamemode buttons
+function openSingleplayerMenu() {
+    singleplayerButton.style.display = 'none';
+    mutliplayerButton.style.display = 'none';
+    singleplayerSettings.style.display = 'block';
+}
+
+// Close singleplayer settings and reveal gamemode buttons
+function closeSingleplayerMenu() {
+    singleplayerButton.style.display = 'inline';
+    mutliplayerButton.style.display = 'inline';
+    singleplayerSettings.style.display = 'none';
+}
